5d404136ad4789dc1dc280c6a4263188f0b49ead
Шмаков Алексей <a.shmakov@axelot.ru>
2016-01-28 12:36:09 +0300

    Парсер Rpm вынесен в отдельный проект dll

---
+++ RpmReaderNet/RpmReaderTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using RpmReaderNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpmReaderTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (RpmReader reader = new RpmReader("esbadapterhost.rpm"))
+            {
+                if (reader.Validate())
+                {
+                    System.Console.WriteLine(reader.Version);
+                }
+            }
+
+        }
+    }
+}
