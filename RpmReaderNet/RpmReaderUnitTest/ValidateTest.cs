b398b2ac67444740088049918421b8518e30fb94
Шмаков Алексей <a.shmakov@axelot.ru>
2016-07-06 14:06:53 +0300

    Продолжение написания unit-теста

--- RpmReaderNet/RpmReaderUnitTest/ValidateTest.cs
+++ RpmReaderNet/RpmReaderUnitTest/ValidateTest.cs
@@ -2,12 +2,30 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RpmReaderNet;
 using System.IO;
+using System.Diagnostics;
 
 namespace RpmReaderUnitTest
 {
     [TestClass]
     public class ValidateTest
     {
+        private TestContext testContextInstance;
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
         /// <summary>
         /// Execute a validation of input file and check metadata from package
         /// </summary>
@@ -26,14 +44,14 @@ namespace RpmReaderUnitTest
                 Assert.AreEqual(reader.BuildHost, "copr-builder-856817140.novalocal");
                 Assert.AreEqual(reader.Summary, "The cross-platform open-source multimedia framework, player and server");
                 Assert.AreEqual(reader.Vendor, "Fedora Project COPR (paulcarroty/test-3.0.2)");
-                Assert.AreEqual(reader.SourceRpm, null);
-
-                //Assert.AreEqual(reader.Description, "VLC media player is a highly portable multimedia player and multimedia framework capable of reading most audio and video formats as well as DVDs, Audio CDs VCDs, and various streaming protocols. It can also be used as a media converter or a server to stream in uni - cast or multi - cast in IPv4 or IPv6 on networks.");
-                //Assert.AreEqual(reader.Size, 34215049);
+                Assert.AreEqual(reader.SourceRpm, null);               
+                Assert.AreEqual(reader.Description, "VLC media player is a highly portable multimedia player and multimedia framework\ncapable of reading most audio and video formats as well as DVDs, Audio CDs VCDs,\nand various streaming protocols.\nIt can also be used as a media converter or a server to stream in uni-cast or\nmulti-cast in IPv4 or IPv6 on networks.");
+                uint expectedSize = 34215049;
+                Assert.AreEqual(reader.Size, expectedSize);
                 //Assert.AreEqual(reader.BuildTime, new DateTime(2016, 06, 26, ))
 
 
-                Assert.AreEqual(reader.Changelog, File.ReadAllText("changelog_standart.txt"));
+               // Assert.AreEqual(reader.Changelog, File.ReadAllText("changelog_standart.txt"));
 
                 
          
